b26ea175ba135ff081dd91cd70a005af647937cd
Valtýr Kári Daníelsson <vkd1@hi.is>
2023-08-15 03:31:37 +0000

    adds dispersion plotting capabilities

--- gpsim.py
+++ gpsim.py
@@ -1,13 +1,16 @@
 from solvers import SsfmGP, normSqr, gauss
 import numpy as np
+import subprocess
 import matplotlib.pyplot as plt
 from matplotlib import cm, animation
 from numba import jit
 import argparse
 
-plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+ffmpegPath = subprocess.run(["whereis", "ffmpeg"], capture_output=True, encoding='utf8')
+plt.rcParams['animation.ffmpeg_path'] = ffmpegPath.stdout.split()[1]
 parser = argparse.ArgumentParser()
-parser.add_argument('filename')
+parser.add_argument('-a', '--animation')
+parser.add_argument('-d', '--dispersion')
 args = parser.parse_args()
 
 
@@ -24,16 +27,18 @@ samples = 256
 dx = (endX - startX) / samples
 x = np.arange(startX, endX, dx)
 xv, yv = np.meshgrid(x, x)
+k0x = np.pi / dx
 # *(1-(xv/5)**6*(yv/5)**6)
 
 dt = 0.01
-nframes = 400
+nframes = 512
 fps = 12
 t = dt * np.arange(1, nframes+1)
 psi0 = np.random.rand(samples, samples).astype('complex')
+#psi0 = np.zeros((samples, samples), dtype='complex')
 pump = 200 * (gauss(xv - 5, yv).astype('complex') + gauss(xv + 5, yv).astype('complex'))
 nR = np.zeros((samples, samples))
-alpha = 1
+alpha = 0.01
 gammalp = 2
 Gamma = 2
 G = 0.1
@@ -54,6 +59,7 @@ def V(x, y, wavefunction, nR):
 
 
 gpsim = SsfmGP(psi0, xv, yv, 0.5, V, nR=nR, gamma=Gamma, R=R, pump=pump, dt=0.01)
+snapshots = np.zeros((512, 256), dtype='complex')
 fig, ax = plt.subplots()
 im = ax.imshow(normSqr(gpsim.psi),
                cmap=cm.viridis,
@@ -73,6 +79,7 @@ def animate_heatmap(frame):
     ax.set_title(f"t = {gpsim.t:.3f}")
     im.set_data(normSqr(gpsim.psi))
     im.set_clim(vmin, vmax)
+    snapshots[frame, :] = gpsim.psi[127, :]
     return [im]
 
 
@@ -84,4 +91,16 @@ anim = animation.FuncAnimation(fig,
 FFwriter = animation.FFMpegWriter(fps=fps,
                                   metadata={'copyright': 'Public Domain'})
 
-anim.save(args.filename, writer=FFwriter)
+anim.save(args.animation, writer=FFwriter)
+spectrum = np.fft.fft2(snapshots)
+plt.cla()
+plt.clf()
+fig, ax = plt.subplots()
+im = ax.imshow(np.log(np.abs(spectrum)),
+               cmap=cm.viridis,
+               origin='lower',
+               aspect='auto',
+               extent=[-k0x, k0x, 0, nframes * dt])
+fig.colorbar(im, ax=ax)
+
+plt.savefig(args.dispersion)
