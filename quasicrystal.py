4b3dbde3474c438a2bbb611353aaf1b43a6dacd0
Valtýr Kári Daníelsson <vkd1@hi.is>
2023-10-11 17:05:00 +0000

    Less intense pumps for quasicrystal

--- quasicrystal.py
+++ quasicrystal.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from src.solvers import SsfmGPCUDA
 from src.penrose import makeSunGrid
+from numpy.fft import fft, ifft, fftshift
 import matplotlib.pyplot as plt
 from matplotlib import animation
 #import argparse
@@ -19,11 +20,6 @@ def gauss(x, y, sigmax, sigmay):
     return torch.exp(-x * x / sigmax - y * y / sigmay)
 
 
-def exactsol(x, y, t, a, m):
-    return (a / (a + 1.0j * t / m)) \
-        * torch.exp(-(x * x + y * y) / (2 * (a + 1.0j * t / m)))
-
-
 cuda = torch.device('cuda')
 samplesX = 512
 samplesY = 512
@@ -34,22 +30,24 @@ endY = 40
 dx = (endX - startX) / samplesX
 dy = (endY - startY) / samplesY
 x = torch.arange(startX, endX, dx)
+y = torch.arange(startY, endY, dy)
 x = x.type(dtype=torch.cfloat)
-gridY, gridX = torch.meshgrid(x, x, indexing='ij')
+y = y.type(dtype=torch.cfloat)
+gridY, gridX = torch.meshgrid(y, x, indexing='ij')
 kxmax = np.pi / dx
 kymax = np.pi / dy
-dt = 0.05
+dt = 0.07
 m = 0.5
-psi = torch.sin(gridX)*torch.cos(gridY)
+psi = 0.1*torch.sin(gridX)*torch.cos(gridY)
 
 # Coefficients for GP equation
-alpha = 0.01
+alpha = 0.1
 gammalp = 2
 Gamma = 1
 G = 0.1
 R = 2
 eta = 1
-constV = ((gridX / 30)**2 + (gridY / 30)**2)**8 - 0.5j*gammalp
+constV = ((gridX / 25)**2 + (gridY / 25)**2)**8 - 0.5j*gammalp
 
 
 def normSqr(x):
@@ -57,9 +55,9 @@ def normSqr(x):
 
 
 points = makeSunGrid(20, 5)
-pump = torch.zeros((samplesX, samplesY), dtype=torch.cfloat)
+pump = torch.zeros((samplesY, samplesX), dtype=torch.cfloat)
 for p in points:
-    pump += 100*gauss(gridX - p.real, gridY - p.imag, 0.1, 0.1)
+    pump += 5*gauss(gridX - p.real, gridY - p.imag, 0.1, 0.1)
 nR = torch.zeros((samplesY, samplesX), dtype=torch.cfloat)
 
 gpsim = SsfmGPCUDA(psi0=psi,
@@ -77,18 +75,16 @@ gpsim = SsfmGPCUDA(psi0=psi,
                    constV=constV,
                    dt=dt)
 
-nframes = 1024
+nframes = 2048
 fps = 24
 fig, ax = plt.subplots()
 im = ax.imshow(normSqr(gpsim.psi).real.cpu().detach().numpy(),
                origin='lower',
                extent=[startX, endX, startY, endY])
-vmin = 0
-vmax = 120
-im.set_clim(vmin, vmax)
 ax.set_xlabel(r'x ($\mu$m)')
 ax.set_ylabel(r'y ($\mu$m)')
 fig.colorbar(im, ax=ax)
+bleh = np.zeros((2048, 512), dtype=complex)
 
 
 def init():
@@ -97,8 +93,12 @@ def init():
 
 def animate_heatmap(frame):
     gpsim.step()
-    data = normSqr(gpsim.psi).real.detach().cpu().numpy()
-    im.set_data(data)
+    data = gpsim.psi.detach().cpu().numpy()
+    bleh[frame, :] = data[127, :]
+    im.set_data(normSqr(data).real)
+    # vmin = np.min(data)
+    # vmax = np.max(data)
+    im.set_clim(0, 5)
     return [im]
 
 
@@ -108,4 +108,13 @@ anim = animation.FuncAnimation(fig,
                                frames=nframes)
 FFwriter = animation.FFMpegWriter(fps=fps,
                                   metadata={'copyright': 'Public Domain'})
-anim.save('animations/quasicrystal.mp4', writer=FFwriter)
+anim.save('animations/lessintensequasicrystal.mp4', writer=FFwriter)
+
+Emax = np.pi / dt
+bleh = fftshift(fft(ifft(bleh, axis=0), axis=1))
+plt.cla()
+fig, ax = plt.subplots()
+im = ax.imshow(np.log(normSqr(bleh).real),
+               aspect='auto',
+               extent=[-kxmax, kxmax, -Emax, Emax])
+plt.savefig('dispersion.pdf')
